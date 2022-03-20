97ad8a1812de9c6ac4cdcb765fb371c5d254a6ab
William Killerud <william@killerud.com>
2022-03-20 16:03:30 +0100

    Meta

--- app/root.tsx
+++ app/root.tsx
@@ -5,14 +5,25 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "remix";
-import type { MetaFunction } from "remix";
+} from 'remix';
+import type { MetaFunction } from 'remix';
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+export const meta: MetaFunction = () => {
+  const title = 'F in the Chat';
+  const description = 'Paying respects as a Service!';
+  return {
+    title: 'F in the Chat',
+    viewport: 'width=device-width,initial-scale=1',
+    charset: 'utf-8',
+    description,
+    keywords: 'Remix,jokes',
+    'og:title': title,
+    'og:description': description,
+    'og:type': 'website',
+    'twitter:title': title,
+    'twitter:description': description,
+  };
+};
 
 export default function App() {
   return (
