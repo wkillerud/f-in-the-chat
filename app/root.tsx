50547b374b53f69a5e191553cc645998f6f2b88d
William Killerud <william@killerud.com>
2022-04-15 17:16:49 +0200

    Optimaliser litt for the socials

--- app/root.tsx
+++ app/root.tsx
@@ -9,19 +9,23 @@ import {
 import type { MetaFunction } from 'remix';
 
 export const meta: MetaFunction = () => {
-  const title = 'F in the Chat';
-  const description = 'Paying respects as a Service!';
+  const title = 'F in the Chat - Paying respects as a Service!';
+  const description = 'A randomly generated F, any time and any place you need one.';
+  const preview = 'https://finthe.chat/og_image.png';
   return {
-    title: 'F in the Chat',
+    title,
     viewport: 'width=device-width,initial-scale=1',
     charset: 'utf-8',
     description,
-    keywords: 'Remix,jokes',
+    keywords: 'Remix,jokes,silly,chat,f,respects',
     'og:title': title,
     'og:description': description,
     'og:type': 'website',
+    'og:image': preview,
+    'twitter:card': 'summary_large_image',
     'twitter:title': title,
     'twitter:description': description,
+    'twitter:image': preview,
   };
 };
 
